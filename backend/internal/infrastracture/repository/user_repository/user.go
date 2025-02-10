19caa4cbfd6dd0cb114eb7b6e04694c454867579
taga3s <bepozurapu274@gmail.com>
2025-02-10 22:28:10 +0900

    fix(backend): check existed user handling

--- backend/internal/infrastracture/repository/user_repository/user.go
+++ backend/internal/infrastracture/repository/user_repository/user.go
@@ -1,6 +1,7 @@
 package user_repository
 
 import (
+	"errors"
 	um "linguarenz/internal/model/user_model"
 
 	"gorm.io/gorm"
@@ -8,6 +9,7 @@ import (
 
 type IUserRepository interface {
 	FindById(id string) (*um.User, error)
+	FindByGoogleAccountID(googleID string) (*um.User, error)
 	Create(user *um.User) error
 }
 
@@ -21,13 +23,40 @@ func NewUserRepository(db *gorm.DB) IUserRepository {
 
 func (ur *UserRepository) FindById(id string) (*um.User, error) {
 	user := &um.User{}
-	if err := ur.db.Where("id = ?", id).First(user).Error; err != nil {
-		return nil, err
+	result := ur.db.First(user, id)
+	// Check if user not found
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return nil, nil
+	}
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return user, nil
+}
+
+func (ur *UserRepository) FindByGoogleAccountID(googleID string) (*um.User, error) {
+	user := &um.User{}
+	result := ur.db.Where("google_account_id = ?", googleID).First(user)
+	// Check if user not found
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return nil, nil
+	}
+	if result.Error != nil {
+		return nil, result.Error
 	}
 	return user, nil
 }
 
 func (ur *UserRepository) Create(user *um.User) error {
+	// Check if user already exists
+	existedUser, err := ur.FindByGoogleAccountID(user.GoogleAccountID)
+	if err != nil {
+		return err
+	}
+	if existedUser != nil {
+		return nil
+	}
+	// Create user
 	if err := ur.db.Create(user).Error; err != nil {
 		return err
 	}
