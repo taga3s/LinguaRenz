8c3a7fa057e8f22c1e7521bf02ec567e712d7eef
mamenz752 <mamenz752@gmail.com>
2025-02-11 16:24:04 +0900

    wip(frontend): SVG processing for icons

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,7 +3,10 @@ import { StackProvider, StackTheme } from "@stackframe/stack";
 import { stackServerApp } from "../stack";
 import { notoSans } from "./utils/fonts";
 import "./globals.css";
+// Test
 import { Logo } from "./public/Logo";
+import { BookmarksIcon } from "./public/icons/BookmarksIcon";
+import Flag from "react-flagpack";
 
 export const metadata: Metadata = {
 	title: "LinguaRenz",
@@ -19,6 +22,8 @@ export default function RootLayout({
 		<html lang="ja">
 			<body className={`${notoSans.variable} antialiased`}>
 				<Logo per={50} />
+				<BookmarksIcon />
+				<Flag code="JP" hasBorder={false} />
 				<StackProvider app={stackServerApp}>
 					<StackTheme>{children}</StackTheme>
 				</StackProvider>
---
+++ frontend/src/app/public/icons/colors.ts
@@ -0,0 +1,2 @@
+export const LRZ_BLUE = "#1290DE";
+export const LRZ_PURPLE = "#AB24E5";
