35c6743ae5d3c63cfccec74ade6c2b19550c7cb4
taga3s <bepozurapu274@gmail.com>
2025-02-12 18:51:03 +0900

    refactor(frontend): wrap signin logic with handleClickSignIn

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -6,15 +6,17 @@ import { GoogleBlandIcon } from "@/assets/icons/GoogleBrandIcon";
 export default function LoginPage() {
 	const app = useStackApp();
 
+	const handleClickSignIn = async () => {
+		await app.signInWithOAuth("google");
+	};
+
 	return (
 		<div className="flex flex-col items-center justify-center h-screen gap-16">
 			<Logo per={100} />
 			<p className="font-zen tracking-wider text-lg">「検索」で世界を覗こう</p>
 			<button
 				type="button"
-				onClick={async () => {
-					await app.signInWithOAuth("google");
-				}}
+				onClick={handleClickSignIn}
 				className="bg-white border border-lrz-border-gray flex items-center justify-center gap-8 font-zen tracking-wider px-24 py-5 rounded-sm shadow-sm"
 			>
 				<GoogleBlandIcon />
