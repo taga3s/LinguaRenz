f2aed0c652965d240ab8e43781733c5b0be2473b
mamenz752 <mamenz752@gmail.com>
2025-02-12 15:41:16 +0900

    feature(frontend): create SideTab component

--- frontend/src/assets/icons/NewSearchTabMenuIcon.tsx
+++ frontend/src/assets/icons/NewSearchTabMenuIcon.tsx
@@ -4,8 +4,8 @@ export function NewSearchTabMenuIcon() {
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
-			width="24"
-			height="24"
+			width="32"
+			height="32"
 			viewBox="0 0 24 24"
 		>
 			<title>add_square_line</title>
--- frontend/src/components/sidetab/button/SideTabButton.tsx
+++ frontend/src/components/sidetab/button/SideTabButton.tsx
@@ -1,7 +1,52 @@
-export default function SideTabButton() {
+import { BookmarksMenuIcon } from "@/assets/icons/BookmarksMenuIcon";
+import { FlagMenuIcon } from "@/assets/icons/FlagMenuIcon";
+import { SquareLineMenuIcon } from "@/assets/icons/SquareLineMenuIcon";
+import { NewSearchTabMenuIcon } from "@/assets/icons/NewSearchTabMenuIcon";
+import { UserMenuIcon } from "@/assets/icons/UserMenuIcon";
+
+type props = {
+    buttonType: string | null;
+    buttonName: string | null;
+}
+
+export default function SideTabButton(props: props) {
+    const { buttonType, buttonName } = props;
+    const changeButtonIcons = (buttonType: string | null) => {
+        switch (buttonType) {
+            case "article":
+                return <BookmarksMenuIcon />
+            case "history":
+                return <FlagMenuIcon />
+            case "newTab":
+                return <NewSearchTabMenuIcon />
+            case "settings":
+                return <UserMenuIcon />
+            default:
+                return <SquareLineMenuIcon />
+        }
+    }
+    const changeButtonName = (buttonType: string | null, buttonName: string | null) => {
+        switch (buttonType) {
+            case "article":
+                return "記事ブックマーク"
+            case "history":
+                return "検索履歴ブックマーク"
+            case "newTab":
+                return "新しい検索"
+            case "settings":
+                return "mail@mail.com"
+            default:
+                return buttonName;
+        }
+    }
+
     return (
-        <div>
-            <h1>SideTabButton</h1>
-        </div>
+        // TODO: change next/link component
+        <button className="px-8 py-4 w-full flex items-center justify-start gap-6 rounded-sm hover:bg-lrz-border-gray">
+            {changeButtonIcons(buttonType)}
+            <h1 className="font-zen font-bold tracking-wider">
+                {changeButtonName(buttonType, buttonName)}
+            </h1>
+        </button>
     );
 }
