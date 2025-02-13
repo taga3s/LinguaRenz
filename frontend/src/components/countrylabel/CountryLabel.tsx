21efd92adbdaa680687479b1a8f8ad76fa4c3be1
mamenz752 <mamenz752@gmail.com>
2025-02-13 19:59:44 +0900

    fix(frontend): format and lint check

--- frontend/src/components/countrylabel/CountryLabel.tsx
+++ frontend/src/components/countrylabel/CountryLabel.tsx
@@ -1,36 +1,34 @@
-import Flag from "react-flagpack"
+import Flag from "react-flagpack";
 
 type props = {
-    code: string
-    size?: "S" | "M" | "L"
-    countryName: string
-    hasBorder: boolean
-}
+	code: string;
+	size?: "S" | "M" | "L";
+	countryName: string;
+	hasBorder: boolean;
+};
 
 export function CountryLabel(props: props) {
-    const { code, size, countryName, hasBorder } = props;
-    const hasBorderClass = (hasBorder: boolean) => {
-        if (hasBorder) {
-            return "rounded-full bg-lrz-bg-white border border-lrz-border-gray"
-        }
-    }
+	const { code, size, countryName, hasBorder } = props;
+	const hasBorderClass = (hasBorder: boolean) => {
+		if (hasBorder) {
+			return "rounded-full bg-lrz-bg-white border border-lrz-border-gray";
+		}
+	};
 
-    return (
-        <div
-            className={
-                `flex gap-4 px-4 py-2 items-center
-                ${hasBorderClass(hasBorder)}`
-            }
-        >
-            <div className="border border-lrz-border-gray">
-                <Flag
-                    code={code}
-                    size={size ? size : "L"} 
-                    hasDropShadow={true}
-                    hasBorder={false}
-                />
-            </div>
-            <p className="tracking-wider">{countryName}</p>
-        </div>
-    )
+	return (
+		<div
+			className={`flex gap-4 px-4 py-2 items-center
+                ${hasBorderClass(hasBorder)}`}
+		>
+			<div className="border border-lrz-border-gray">
+				<Flag
+					code={code}
+					size={size ? size : "L"}
+					hasDropShadow={true}
+					hasBorder={false}
+				/>
+			</div>
+			<p className="tracking-wider">{countryName}</p>
+		</div>
+	);
 }
